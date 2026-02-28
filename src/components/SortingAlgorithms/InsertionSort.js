54cda087fb10667d28b0cbed5d58926c4083b275
Max Tretter <maxtretter@mailbox.org>
2026-02-28 18:28:17 +0100

    fix: correct insertion sort loop logic

--- src/components/SortingAlgorithms/InsertionSort.js
+++ src/components/SortingAlgorithms/InsertionSort.js
@@ -2,17 +2,15 @@ import { sleep } from "../Helper/helper";
 
 export async function InsertionSort(array, setArray) {
   let temp, j;
-  for (let index = 0; index < array.length; index++) {
-    for (let i = 1; i < array.length; i++) {
-      temp = array[i];
-      j = i - 1;
-      while (j >= 0 && array[j] > temp) {
-        array[j + 1] = array[j];
-        j--;
-      }
-      array[j + 1] = temp;
-      await sleep(500);
-      setArray([...array]);
+  for (let i = 1; i < array.length; i++) {
+    temp = array[i];
+    j = i - 1;
+    while (j >= 0 && array[j] > temp) {
+      array[j + 1] = array[j];
+      j--;
     }
+    array[j + 1] = temp;
+    await sleep(500);
+    setArray([...array]);
   }
 }
